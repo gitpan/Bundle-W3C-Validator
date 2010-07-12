3bd271a6d1ff7a22da3bee9eb2f7b887368817b5
Ville Skyttä <ville.skytta@iki.fi>
2010-07-12 17:23:14 +0000

    Import of SCOP/Bundle-W3C-Validator-1.1 from CPAN.
    gitpan-cpan-distribution: Bundle-W3C-Validator
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         SCOP/Bundle-W3C-Validator-1.1.tar.gz
    gitpan-cpan-author:       SCOP
    gitpan-cpan-maturity:     released

--- lib/Bundle/W3C/Validator.pm
+++ lib/Bundle/W3C/Validator.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = '1.0';
+$VERSION = '1.1';
 
 1;
 
@@ -49,6 +49,7 @@ C<perl -MCPAN -e "install Bundle::W3C::Validator">
  URI
  URI::Escape
  URI::file
+ URI::Heuristic
  XML::LibXML 1.70
 
 =head1 DESCRIPTION
